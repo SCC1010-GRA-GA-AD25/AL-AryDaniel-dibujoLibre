114681219cad6025b5126b3d8488aea674261ad4
AryDaniel <ary.lopez@cetis107.edu.mx>
2025-09-22 14:27:53 -0700

    feat(text): add custom fonts and display quote in drawText function

--- sketch.js
+++ sketch.js
@@ -1,24 +1,45 @@
 function preload() {
+  myFontRegular = loadFont("assets/IMFellEnglish-Regular.ttf");
+  myFontItalic = loadFont("assets/IMFellEnglish-Italic.ttf");
 }
 
 function setup() { 
   cnv = createCanvas(400, 550);
   centerCanvas();
+  
 }
 
 function draw() {
   background(31, 131, 173);
+
+  drawText();
 
   // Draw card in middle
   fill(255);
   stroke(0);
   strokeWeight(4);
   rect(10, 10, 380, 350, 20);
-
 }
 
 function centerCanvas() {
   let x = (windowWidth - width) / 2;
   let y = (windowHeight - height) / 2;
   cnv.position(x, y);   // moves the canvas
+}
+
+function drawText() {
+  textFont(myFontRegular);
+  textSize(24);
+
+  fill(0);
+  noStroke();
+  textSize(16);
+  text(
+    "The lion cannot protect himself from traps,\nand the fox cannot defend himself from wolves.\nOne must therefore be a fox to recognize traps,\nand a lion to frighten wolves.",
+    30,
+    height - 120,
+  );
+  
+  textFont(myFontItalic);
+  text("- Niccolò Machiavelli", 250, height - 30);
 }
