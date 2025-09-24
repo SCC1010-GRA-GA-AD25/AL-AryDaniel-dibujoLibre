f198cc1e78e630a749e65273e3107f274400ff91
AryDaniel <ary.lopez@cetis107.edu.mx>
2025-09-24 11:32:45 -0700

    feat(ui): draw stylized lion using shapes

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function draw() {
   fill(255);
   stroke(0);
   strokeWeight(4);
-  fill(127)
+  fill(0)
   rect(10, 10, 380, 350, 20);
 
   
@@ -28,6 +28,9 @@ function draw() {
 
   // Draw Fox
   drawFox();
+  
+    // Draw Lion
+    drawLion();
 }
 
 function centerCanvas() {
@@ -113,4 +116,179 @@ function drawFox() {
     vertex(180, 225);
     vertex(175, 223);
   endShape(CLOSE);  
+}
+
+function drawLion() {
+  strokeWeight(0);
+  // -------------- Melena -----------------
+  fill(242, 243, 174) // blanco (nariz ojo)
+  fill(221, 142, 62) // crema (cara claro)
+
+  
+  fill(198, 145, 69) // brown medium
+    quad(215, 120, 215, 70, 285, 30, 275, 70); // Nivel 3
+    triangle(200, 70, 200, 96, 215, 60); 
+    beginShape(); // Nivel 1
+    vertex(280, 88);
+    vertex(300, 95);
+    vertex(320, 130);
+    vertex(230, 110);
+    endShape(CLOSE);
+    
+    triangle(260, 190, 320, 280, 306, 230) // 1
+    
+    triangle(200, 270, 200, 295, 210, 275)
+    quad(200, 300, 200, 345, 210, 340, 215, 280)
+    quad(223, 243, 230, 250, 250, 310, 240, 320); // 
+    quad(240, 245, 260, 200, 278, 240, 260, 300); // 2
+    quad(296, 250, 299, 285, 275, 320, 275, 225); // 3
+    square(250, 165, 55) // 3
+    
+  fill(179, 120, 46) // brown dark
+    triangle(213, 125, 242, 90, 218, 105); 
+    triangle(200, 115, 200, 95, 226, 69); // melena top
+    beginShape(); // 2 melena right L
+      vertex(273, 165)
+      vertex(270, 135)
+      vertex(310, 150)
+      vertex(325, 210)
+      vertex(300, 175)
+    endShape(CLOSE);
+    quad(270, 205, 270, 195, 320, 220, 320, 240)
+    triangle(250, 170, 280, 220, 275, 250)
+      
+    triangle(230, 250, 240, 280, 244, 240)
+
+    beginShape() // 2 - melena abajo
+      vertex(210, 275)
+      vertex(217, 271)
+      vertex(233, 285)
+      vertex(238, 310)
+      vertex(233, 320)
+      vertex(235, 323)
+      vertex(230, 355)
+      vertex(220, 330)
+      vertex(220, 315)
+      vertex(202, 289)
+    endShape(CLOSE)
+      
+  fill(220, 100, 5); // Naranja oscuro - fox
+    beginShape(); // Nivel 2 - cafe oscuro - melena costado
+      vertex(295, 250);
+      vertex(300, 300);
+      vertex(310, 264);
+      vertex(280, 220);
+      vertex(255, 210);
+    endShape(CLOSE);
+    
+    triangle(247, 110, 290, 120, 300, 175)
+    
+    beginShape(); // Nivel 2
+    vertex(250, 80);
+    vertex(290, 75);
+    vertex(275, 95);
+    vertex(260, 100);
+    vertex(235, 115);
+    vertex(220, 115);
+  endShape(CLOSE);
+
+  beginShape(); // Nivel 1
+  vertex(220, 50);
+  vertex(240, 40);
+  vertex(260, 35);
+    vertex(245, 45);
+    vertex(230, 65);
+    vertex(200, 95);
+    endShape(CLOSE);
+    
+    
+    triangle(200, 290, 220, 280, 200, 310) // 3 abajo melena
+    
+    triangle(215, 270, 228, 260, 232, 284)
+    
+    quad(240, 280, 240, 245, 265, 310, 256, 330); // 1
+  
+    triangle(200, 115, 215, 105, 220, 80); 
+
+  // -----------------------------------------------------
+  // -------------- Cuerpo -------------------------------
+  // -----------------------------------------------------
+    
+  fill(255, 170, 0); // Amarillo
+    quad(245, 152, 265, 130, 275, 145, 200, 220);
+  
+    
+    beginShape();
+    vertex(200, 190);
+    vertex(200, 150);
+    vertex(207, 146);
+    vertex(220, 165);
+    vertex(215, 185);
+    vertex(223, 205);
+    endShape(CLOSE);
+
+
+  fill(222, 143, 18)
+    quad(217, 110, 250, 112, 240, 195, 207, 146); // fore head
+    
+  fill(255, 153, 51); // naranja
+    triangle(245, 152, 250, 112, 265, 130);
+    square(200, 210, 35) // above nouse
+    
+    beginShape(); // cheek
+      vertex(220, 165);
+      vertex(235, 153);
+      vertex(260, 155);
+      vertex(275, 145);
+      vertex(273, 175);
+      vertex(280, 205);
+      vertex(250, 240);
+      vertex(240, 230);
+      vertex(230, 225);
+      vertex(215, 185);
+    endShape(CLOSE);
+
+    beginShape(); // chin
+      vertex(228, 265);
+      vertex(230, 253);
+      vertex(200, 253);
+      vertex(200, 275);
+      vertex(210, 275);
+    endShape(CLOSE);
+  
+  fill(255);
+    beginShape(); // muzzle
+      vertex(200, 253);
+      vertex(200, 238);
+      vertex(205, 233);
+      vertex(220, 230);
+      vertex(228, 218);
+      vertex(240, 220);
+      vertex(245, 228);
+      vertex(244, 245);
+      vertex(236, 253);
+      vertex(210, 257);
+    endShape(CLOSE);
+
+    
+  fill(110, 70, 43) // really dark brown
+    quad(200, 190, 230, 209, 220, 232, 200, 220);
+    quad(200, 115, 200, 150, 207, 146, 220, 100);
+    
+    beginShape(); // cheek
+      vertex(235, 180);
+      vertex(230, 188);
+      vertex(233, 170);
+      vertex(245, 165);
+      vertex(260, 165);
+    endShape(CLOSE);
+
+    fill(255)
+    beginShape();
+      vertex(238, 173);
+      vertex(250, 170);
+      vertex(249, 176);
+      vertex(244, 180);
+      vertex(239, 178);
+    endShape(CLOSE);
 }
